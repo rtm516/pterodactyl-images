352ae8a047c7da0f34e48dc97db457924da11346
Ryan Milner <ryantmilner@hotmail.co.uk>
2019-06-10 10:51:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,40 +7,40 @@ java -version
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
 # Update the server
-apk add --no-cache --update curl jq
 if [ -n "${DL_PATH}" ]; then
 	echo -e "using supplied download url"
 	DOWNLOAD_URL=`eval echo $(echo ${DL_PATH} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 else
-	VER_EXISTS=`curl -s https://papermc.io/api/v1/paper | jq -r --arg VERSION $MINECRAFT_VERSION '.versions[] | IN($VERSION)' | grep true`
-	LATEST_PAPER_VERSION=`curl -s https://papermc.io/api/v1/paper | jq -r '.versions' | jq -r '.[0]'`
-	if [ "${VER_EXISTS}" == "true" ]; then
+	VERSIONS=`curl -s https://papermc.io/api/v1/paper`
+	LATEST_PAPER_VERSION=$(echo $VERSIONS | grep -Po '(?<=\"versions\"\:\[\")(.*?)(?=\")')
+	if [[ $VERSIONS == *"\"${MINECRAFT_VERSION}\""* ]]; then
 		echo -e "Version is valid. Using version ${MINECRAFT_VERSION}"
 	else
 		echo -e "Using the latest paper version"
 		MINECRAFT_VERSION=${LATEST_PAPER_VERSION}
 	fi
-	BUILD_EXISTS=`curl -s https://papermc.io/api/v1/paper/${MINECRAFT_VERSION} | jq -r --arg BUILD ${BUILD_NUMBER} '.builds.all[] | IN($BUILD)' | grep true`
-	LATEST_PAPER_BUILD=`curl -s https://papermc.io/api/v1/paper/${MINECRAFT_VERSION} | jq -r '.builds.latest'`
-	if [ "${BUILD_EXISTS}" == "true" ] || [ ${BUILD_NUMBER} == "latest" ]; then
+	
+	BUILDS=`curl -s https://papermc.io/api/v1/paper/${MINECRAFT_VERSION}`
+	LATEST_PAPER_BUILD=$(echo $BUILDS | grep -Po '(?<=\"latest\"\:\")(.*?)(?=\")')
+	if [ $BUILDS == *"\"${BUILD_NUMBER}\""* ] || [ ${BUILD_NUMBER} == "latest" ]; then
 		echo -e "Build is valid. Using version ${BUILD_NUMBER}"
 	else
 		echo -e "Using the latest paper build"
 		BUILD_NUMBER=${LATEST_PAPER_BUILD}
 	fi
+	
 	echo "Version being downloaded"
 	echo -e "MC Version: ${MINECRAFT_VERSION}"
 	echo -e "Build: ${BUILD_NUMBER}"
 	DOWNLOAD_URL=https://papermc.io/api/v1/paper/${MINECRAFT_VERSION}/${BUILD_NUMBER}/download
 fi
-cd /home/container
 echo -e "running curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}"
 curl -o ${SERVER_JARFILE} ${DOWNLOAD_URL}
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
